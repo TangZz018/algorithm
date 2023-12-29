d5336148a5c2f713c478909d7e813f715630744f
Konieg <95561392+Konieg@users.noreply.github.com>
2023-12-29 20:26:43 +0800

    Add files via upload
    make the following progresses: get total sum and max float with the joint work of 2 computers by using TCP to communicate.

--- projekt_klient_TCP/Clt.cpp
+++ projekt_klient_TCP/Clt.cpp
@@ -7,50 +7,11 @@
 #include "immintrin.h" // avx
 #include <WinSock2.h>
 #include <string>
-
-#define MAX_THREADS 64
-#define SUBDATANUM 1000000
-#define DATANUM (SUBDATANUM * MAX_THREADS)   
-float sumSpeedUp1(const float data[], const int len) {
-    //assert(len % 8 == 0 && "要求N为8的倍数");
-    if (len % 8)
-        std::cout << "要求N为8的倍数" << std::endl;
-
-    float result = 0;
-    int per_iter = len / 8;
-
-    __m256 sum = _mm256_setzero_ps();
-    __m256 c = _mm256_setzero_ps();
-    __m256* ptr = (__m256*)data;
-
-
-#pragma omp parallel for
-    for (int i = 0; i < per_iter; ++i) {
-        // 计算 y，减去运行补偿。
-        __m256 right = _mm256_log_ps(_mm256_sqrt_ps(ptr[i]));
-        __m256 y = _mm256_sub_ps(right, c);
-
-        // 计算中间和。
-        __m256 t = _mm256_add_ps(sum, y);
-
-        // 更新运行补偿。
-        c = _mm256_sub_ps(_mm256_sub_ps(t, sum), y);
-#pragma omp critical
-        // 更新累加器。
-        sum = t;
-    }
-
-    float* resultArray = (float*)&sum;
-    result = resultArray[0] + resultArray[1] + resultArray[2] + resultArray[3] +
-        resultArray[4] + resultArray[5] + resultArray[6] + resultArray[7];
-    return result;
-
-}
-
+#include "methods.h"
 
 int main() {
-    float sum1 = 1, sum2 = 0;
-    float max1 = 0, max2 = 0;
+    float sum = -1;
+    float max = -1;
     LARGE_INTEGER start, end;
     float* result = new float[DATANUM];
     float* rawFloatData = new float[DATANUM];
@@ -98,21 +59,16 @@ int main() {
     }
 
     QueryPerformanceCounter(&start);
-    sum1 = sumSpeedUp1(rawFloatData, DATANUM);
+    sum = sumSpeedUpManual(rawFloatData, DATANUM);
 
     while (true) {
         // 从键盘读取 double 值
-        std::cout << "Enter a double value to send (0 to exit): ";
+        // std::cout << "连接成功 ";
 
 
         // 转换为字符串并发送
-        std::string dataStr = std::to_string(sum1);
-        send(Connection, dataStr.c_str(), dataStr.size(), 0);
-
-        if (sum1 == -1) {
-            // 如果输入为0，退出循环
-            continue;
-        }
+        //std::string dataStr = std::to_string(sum1);
+        send(Connection, (char*)&sum, sizeof(sum), NULL);
 
         // 接收服务器返回的状态
         char status;
@@ -125,19 +81,39 @@ int main() {
         }
     }
     QueryPerformanceCounter(&end);
+    std::cout << "Time Consumed : " << (end.QuadPart - start.QuadPart) << std::endl;
+    std::cout << "输出求和结果 : " << sum << std::endl << std::endl;
+
+
+    QueryPerformanceCounter(&start);
+    max = maxSpeedUp(rawFloatData, DATANUM);
+    while (true) {
+        // 从键盘读取 double 值
+        //std::cout << "Enter a double value to send (0 to exit): ";
+
+
+        //转换为字符串并发送
+        //std::string dataStr = std::to_string(sum1);
+        //send(Connection, dataStr.c_str(), dataStr.size(), 0);
+        send(Connection, (char*)&max, sizeof(max), NULL);
+
+        // 接收服务器返回的状态
+        char status;
+        recv(Connection, &status, sizeof(status), 0);
+        std::cout << "Received status: " << status << std::endl;
+
+        if (status == '1') {
+            // 如果状态为1，退出循环
+            break;
+        }
+    }
+    QueryPerformanceCounter(&end);
     //销毁网络连接
     closesocket(Connection);
     WSACleanup();
 
-    //for (int i = 0; i < 10; i++)
-    //{
-    //	std::cout << result[i] << '\t';
-    //}
-    //std::cout << std::endl;
-
-    std::cout << "Time Consumed 1 : " << (end.QuadPart - start.QuadPart) << std::endl;
-    time_consumed1 = end.QuadPart - start.QuadPart;
-    std::cout << "输出求和结果 : " << sum1 << std::endl;
+    std::cout << "Time Consumed : " << (end.QuadPart - start.QuadPart) << std::endl;
+    std::cout << "输出最大值结果 : " << max << std::endl;
 
     // 清理
     closesocket(Connection);
